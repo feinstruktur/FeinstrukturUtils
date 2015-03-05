6fbc69e22a5623e38f4b33f480e3ea393ed6eafe
Sven A. Schmidt <sas@feinstruktur.com>
2015-03-05 11:22:23 +0000

    Extended CG tests

--- FeinstrukturUtils/CoreGraphics+ext.swift
+++ FeinstrukturUtils/CoreGraphics+ext.swift
@@ -10,49 +10,30 @@ import Foundation
 import CoreGraphics
 
 
+// MARK: - GCPoint
+
 public func +(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
     return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
-}
-
-public func -(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
-    return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
-}
-
-public func /(lhs: CGPoint, rhs: CGFloat) -> CGPoint {
-    return CGPoint(x: lhs.x / rhs, y: lhs.y / rhs)
-}
-
-public func /(lhs: CGSize, rhs: CGFloat) -> CGSize {
-    return CGSize(width: lhs.width / rhs, height: lhs.height / rhs)
-}
-
-public func -(lhs: CGPoint, rhs: CGSize) -> CGPoint {
-    return CGPoint(x: lhs.x - rhs.width, y: lhs.y - rhs.height)
 }
 
 public func +(lhs: CGPoint, rhs: (CGFloat, CGFloat)) -> CGPoint {
     return CGPoint(x: lhs.x + rhs.0, y: lhs.y + rhs.1)
 }
 
+public func -(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+    return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+}
+
+public func -(lhs: CGPoint, rhs: CGSize) -> CGPoint {
+    return CGPoint(x: lhs.x - rhs.width, y: lhs.y - rhs.height)
+}
+
 public func -(lhs: CGPoint, rhs: (CGFloat, CGFloat)) -> CGPoint {
     return CGPoint(x: lhs.x - rhs.0, y: lhs.y - rhs.1)
 }
 
-public func +(lhs: CGSize, rhs: CGSize) -> CGSize {
-    return CGSize(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
-}
-
-public func -(lhs: CGSize, rhs: CGSize) -> CGSize {
-    return CGSize(width: lhs.width - rhs.width, height: lhs.height - rhs.height)
-}
-
-public func +(lhs: CGSize, rhs: (CGFloat, CGFloat)) -> CGSize {
-    return CGSize(width: lhs.width + rhs.0, height: lhs.height + rhs.1)
-}
-
-public func -=(inout lhs: CGPoint, rhs: CGPoint) {
-    lhs.x -= rhs.x
-    lhs.y -= rhs.y
+public func /(lhs: CGPoint, rhs: CGFloat) -> CGPoint {
+    return CGPoint(x: lhs.x / rhs, y: lhs.y / rhs)
 }
 
 public func +=(inout lhs: CGPoint, rhs: CGPoint) {
@@ -60,12 +41,9 @@ public func +=(inout lhs: CGPoint, rhs: CGPoint) {
     lhs.y += rhs.y
 }
 
-public func *(lhs: (CGFloat, CGFloat), rhs: CGFloat) -> (CGFloat, CGFloat) {
-    return (lhs.0 * rhs, lhs.1 * rhs)
-}
-
-public func *(lhs: CGFloat, rhs: (CGFloat, CGFloat)) -> (CGFloat, CGFloat) {
-    return rhs * lhs
+public func -=(inout lhs: CGPoint, rhs: CGPoint) {
+    lhs.x -= rhs.x
+    lhs.y -= rhs.y
 }
 
 extension CGPoint {
@@ -74,9 +52,42 @@ extension CGPoint {
     }
 }
 
+
+// MARK: - CGSize
+
+public func +(lhs: CGSize, rhs: CGSize) -> CGSize {
+    return CGSize(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
+}
+
+public func +(lhs: CGSize, rhs: (CGFloat, CGFloat)) -> CGSize {
+    return CGSize(width: lhs.width + rhs.0, height: lhs.height + rhs.1)
+}
+
+public func -(lhs: CGSize, rhs: CGSize) -> CGSize {
+    return CGSize(width: lhs.width - rhs.width, height: lhs.height - rhs.height)
+}
+
+public func /(lhs: CGSize, rhs: CGFloat) -> CGSize {
+    return CGSize(width: lhs.width / rhs, height: lhs.height / rhs)
+}
+
+
+// MARK: - CGRect
+
 extension CGRect {
     public func withPadding(# x: CGFloat, y: CGFloat) -> CGRect {
         return CGRect(origin: self.origin - (x,y), size: self.size + 2 * (x,y))
     }
+}
+
+
+// MARK: - Generic
+
+public func *(lhs: (CGFloat, CGFloat), rhs: CGFloat) -> (CGFloat, CGFloat) {
+    return (lhs.0 * rhs, lhs.1 * rhs)
+}
+
+public func *(lhs: CGFloat, rhs: (CGFloat, CGFloat)) -> (CGFloat, CGFloat) {
+    return rhs * lhs
 }
 
--- FeinstrukturUtilsTests/CoreGraphicsTests.swift
+++ FeinstrukturUtilsTests/CoreGraphicsTests.swift
@@ -11,10 +11,95 @@ import XCTest
 import Nimble
 
 
-class CoreGraphicsTests: XCTestCase {
+let a = CGPoint(x: 1, y: 2)
+let b = CGPoint(x: 2, y: 3)
+let c = CGPoint(x: 3, y: 6)
+let s = CGSize(width: 10, height: 20)
 
-    func testAddition() {
-        expect(CGPoint(x: 1, y: 2) + CGPoint(x: 2, y: 3)) == CGPoint(x: 3, y: 5)
+
+class CGPointTests: XCTestCase {
+
+    func test_addition() {
+        expect(a + b) == CGPoint(x: 3, y: 5)
+        expect(b + a) == CGPoint(x: 3, y: 5)
+        expect(a + (2.0, 3.0)) == CGPoint(x: 3, y: 5)
+        expect(a + (2, 3)) == CGPoint(x: 3, y: 5)
+    }
+    
+    func test_subtraction() {
+        expect(a - c) == CGPoint(x: -2, y: -4)
+        expect(c - b) == CGPoint(x: 1, y: 3)
+        expect(a - s) == CGPoint(x: -9, y: -18)
+        expect(a - (3, 5)) == CGPoint(x: -2, y: -3)
+    }
+    
+    func test_division() {
+        expect(a / 2) == CGPoint(x: 0.5, y: 1)
+    }
+    
+    func test_augmentedAddition() {
+        var x = a
+        x += b
+        expect(x) == a + b
+    }
+    
+    func test_augmentedSubtraction() {
+        var x = a
+        x -= c
+        expect(x) == a - c
+    }
+    
+    func test_initWithSize() {
+        expect(CGPoint(size: s)) == CGPoint(x: 10, y: 20)
     }
     
 }
+
+
+let t = CGSize(width: 15, height: 40)
+
+
+class CGSizeTests: XCTestCase {
+
+    func test_addition() {
+        expect(s + t) == CGSize(width: 25, height: 60)
+        expect(s + (5, 10)) == CGSize(width: 15, height: 30)
+    }
+    
+    func test_subtraction() {
+        expect(s - t) == CGSize(width: -5, height: -20)
+        expect(t - s) == CGSize(width: 5, height: 20)
+    }
+    
+    func test_division() {
+        expect(s / 2) == CGSize(width: 5, height: 10)
+    }
+    
+}
+
+
+class CGRectTests: XCTestCase {
+    
+    func test_withPadding() {
+        let r = CGRect(x: 1, y: 2, width: 5, height: 10)
+        expect(r.withPadding(x: 5, y: 15)) == CGRect(x: -4, y: -13, width: 15, height: 40)
+    }
+    
+}
+
+
+
+class TupleTests: XCTestCase {
+
+    func test_multiplication() {
+        let x: (CGFloat, CGFloat) = (1, 2)
+        var res = 2.5 * x
+        expect(res.0) == 2.5
+        expect(res.1) == 5.0
+        res = x * 2.5
+        expect(res.0) == 2.5
+        expect(res.1) == 5.0
+    }
+    
+}
+
