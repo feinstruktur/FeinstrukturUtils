9a3cc63f269db661f4dbd52a513d89634d4884f8
Sven A. Schmidt <sas@feinstruktur.com>
2015-05-11 09:15:26 +0100

    refactor, all tests pass

--- FeinstrukturUtils/Async.swift
+++ FeinstrukturUtils/Async.swift
@@ -120,12 +120,7 @@ public typealias dispatch_cancelable_closure = (cancel : Bool) -> ()
 public func delay(time:NSTimeInterval, closure:()->()) ->  dispatch_cancelable_closure? {
     
     func dispatch_later(clsr:()->()) {
-        dispatch_after(
-            dispatch_time(
-                DISPATCH_TIME_NOW,
-                Int64(time * Double(NSEC_PER_SEC))
-            ),
-            dispatch_get_main_queue(), clsr)
+        dispatch_after(nowPlus(time), dispatch_get_main_queue(), clsr)
     }
     
     var closure:dispatch_block_t? = closure
